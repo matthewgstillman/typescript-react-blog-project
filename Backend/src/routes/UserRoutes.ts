dc7ffc78d5b97bb25745daa95d903ad0f42c7051
matthewgstillman <matthewgstillman@gmail.com>
2024-07-28 14:04:28 -0600

    User routes updates, express rate limit install

--- Backend/src/routes/UserRoutes.ts
+++ Backend/src/routes/UserRoutes.ts
@@ -1,11 +1,36 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { registerUser, loginUser, getUserProfile, updateUserProfile } from '../controllers/UserController';
 import { protect } from '../middleware/authMiddleware';
+import rateLimit from 'express-rate-limit';
 
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  message: 'Too many accounts created from this IP, please try again after 15 minutes'
+});
+
+router.use('/login', apiLimiter);
+router.use('/register', apiLimiter);
+
+const registrationValidation = [
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
+];
+
+const loginValidation = [
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Password cannot be empty').not().isEmpty(),
+];
+
+router.post('/register', registrationValidation, registerUser);
+
+router.post('/login', loginValidation, loginUser);
+
+router.route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
 
 export default router;
